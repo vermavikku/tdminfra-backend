01a688de585c64fca6929aadc31450655d3426dd
vermavikku <vermavikku1998@gmail.com>
2026-05-10 16:15:08 +0530

    updated multer info

--- src/machineries/machineries.controller.ts
+++ src/machineries/machineries.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query, UseInterceptors, UploadedFile } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+  Query,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MachineriesService } from './machineries.service';
 import { CreateMachineryDto } from './dto/create-machinery.dto';
 import { UpdateMachineryDto } from './dto/update-machinery.dto';
 import { QueryMachineryDto } from './dto/query-machinery.dto';
-import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiQuery, ApiConsumes, ApiBody } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiQuery,
+  ApiConsumes,
+  ApiBody,
+} from '@nestjs/swagger';
 import { Machinery } from './entities/machinery.entity';
 import { S3Service } from '../common/s3/s3.service';
+import * as multer from 'multer';
 
 @ApiTags('machineries')
 @Controller('machineries')
@@ -17,7 +38,14 @@ export class MachineriesController {
   ) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('image'))
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: multer.memoryStorage(),
+      limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+      },
+    }),
+  )
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     schema: {
@@ -38,8 +66,14 @@ export class MachineriesController {
       required: ['title', 'category_code'],
     },
   })
-  @ApiOperation({ summary: 'Create a new machinery with an optional image upload' })
-  @ApiResponse({ status: 201, description: 'The machinery has been successfully created.', type: Machinery })
+  @ApiOperation({
+    summary: 'Create a new machinery with an optional image upload',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The machinery has been successfully created.',
+    type: Machinery,
+  })
   @ApiResponse({ status: 400, description: 'Invalid input.' })
   async create(
     @Body() createMachineryDto: CreateMachineryDto,
@@ -49,27 +83,71 @@ export class MachineriesController {
     if (file) {
       imageUrl = await this.uploadService.uploadFile(file, 'machineries');
     }
-    return this.machineriesService.create({ ...createMachineryDto, image_url: imageUrl });
+    return this.machineriesService.create({
+      ...createMachineryDto,
+      image_url: imageUrl,
+    });
   }
 
   @Get()
-  @ApiOperation({ summary: 'Retrieve all machineries with pagination and filtering' })
-  @ApiQuery({ name: 'page', required: false, type: Number, description: 'Page number (default: 1)' })
-  @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Items per page (default: 10)' })
-  @ApiQuery({ name: 'title', required: false, type: String, description: 'Filter by machinery title' })
-  @ApiQuery({ name: 'category_code', required: false, type: String, description: 'Filter by category code' })
-  @ApiQuery({ name: 'status', required: false, enum: ['active', 'inactive'], description: 'Filter by machinery status' })
-  @ApiResponse({ status: 200, description: 'Successfully retrieved machineries.', type: [Machinery] })
+  @ApiOperation({
+    summary: 'Retrieve all machineries with pagination and filtering',
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'Page number (default: 1)',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description: 'Items per page (default: 10)',
+  })
+  @ApiQuery({
+    name: 'title',
+    required: false,
+    type: String,
+    description: 'Filter by machinery title',
+  })
+  @ApiQuery({
+    name: 'category_code',
+    required: false,
+    type: String,
+    description: 'Filter by category code',
+  })
+  @ApiQuery({
+    name: 'status',
+    required: false,
+    enum: ['active', 'inactive'],
+    description: 'Filter by machinery status',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved machineries.',
+    type: [Machinery],
+  })
   async findAll(@Query() query: QueryMachineryDto) {
     return this.machineriesService.findAll(query);
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Retrieve a machinery by ID' })
-  @ApiParam({ name: 'id', description: 'ID of the machinery to retrieve', type: Number })
-  @ApiResponse({ status: 200, description: 'Successfully retrieved the machinery.', type: Machinery })
+  @ApiParam({
+    name: 'id',
+    description: 'ID of the machinery to retrieve',
+    type: Number,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved the machinery.',
+    type: Machinery,
+  })
   @ApiResponse({ status: 404, description: 'Machinery not found.' })
-  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Machinery | null> {
+  async findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Machinery | null> {
     return this.machineriesService.findOne(id);
   }
 
@@ -94,9 +172,19 @@ export class MachineriesController {
       },
     },
   })
-  @ApiOperation({ summary: 'Update a machinery by ID with an optional image upload' })
-  @ApiParam({ name: 'id', description: 'ID of the machinery to update', type: Number })
-  @ApiResponse({ status: 200, description: 'The machinery has been successfully updated.', type: Machinery })
+  @ApiOperation({
+    summary: 'Update a machinery by ID with an optional image upload',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'ID of the machinery to update',
+    type: Number,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The machinery has been successfully updated.',
+    type: Machinery,
+  })
   @ApiResponse({ status: 404, description: 'Machinery not found.' })
   @ApiResponse({ status: 400, description: 'Invalid input.' })
   async update(
@@ -108,13 +196,24 @@ export class MachineriesController {
     if (file) {
       imageUrl = await this.uploadService.uploadFile(file, 'machineries');
     }
-    return this.machineriesService.update(id, { ...updateMachineryDto, image_url: imageUrl });
+    return this.machineriesService.update(id, {
+      ...updateMachineryDto,
+      image_url: imageUrl,
+    });
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete a machinery by ID' })
-  @ApiParam({ name: 'id', description: 'ID of the machinery to delete', type: Number })
-  @ApiResponse({ status: 200, description: 'The machinery has been successfully deleted.', type: Machinery })
+  @ApiParam({
+    name: 'id',
+    description: 'ID of the machinery to delete',
+    type: Number,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The machinery has been successfully deleted.',
+    type: Machinery,
+  })
   @ApiResponse({ status: 404, description: 'Machinery not found.' })
   async remove(@Param('id', ParseIntPipe) id: number): Promise<Machinery> {
     return this.machineriesService.remove(id);
